7a699cd33b928b9dee9761dbab1cc8c370fdb962
kuro <7445841+kuro7766@user.noreply.gitee.com>
2023-03-28 22:24:14 +0800

    Via script

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -46,7 +46,8 @@ module.exports = {
           // { icon: "reco-bilibili", link: "https://space.bilibili.com/12167681" },
           // { icon: "reco-qq", link: "tencent://message/?uin=2280315050" },
           // { icon: "fa-brands fa-kaggle",link: 'https://www.kaggle.com/galegale05'}
-          { icon: "fa-kaggle",link: 'https://www.kaggle.com/galegale05'}
+          // { icon: "fa-kaggle",link: 'https://www.kaggle.com/galegale05'}
+          { icon: "fab-kaggle",link: 'https://www.kaggle.com/galegale05'}
           // { icon: "reco-twitter", link: "https://twitter.com/a1124851454" },
           // { icon: "reco-mail", link: "mailto:a1124851454@gmail.com" },
         ],
